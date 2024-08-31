f3bd5ab0a575e62df4eeef7b2fec1859015c8463
Rayhan Ferdous Faisal <rayhanferdous07@yahoo.com>
2024-09-01 03:37:02 +0600

    Initial commit

---
+++ TicTac/GameState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTac
+{
+    public class GameState
+    {
+        public Player[,] GameGrid { get; private set; }
+        public Player CurrentPlayer { get; private set; }
+        public int TurnsCount { get; private set; }
+        public bool GameOver { get; private set; }
+
+
+        public event Action<int, int> MoveMade;
+        public event Action<GameResult> GameEnded;
+        public event Action GameRestarted;
+
+        public GameState()
+        {
+            GameGrid = new Player[3, 3];
+            CurrentPlayer = Player.X;
+            TurnsCount = 0;
+            GameOver = false;
+        }
+
+        private bool CanMakeMove(int r, int c)
+        {
+            return !GameOver && GameGrid[r, c] == Player.None;
+        }
+
+        private bool IsGridFull()
+        {
+            return TurnsCount == 9;
+        }
+        
+        private void SwitchPlayer()
+        {
+            CurrentPlayer = (CurrentPlayer == Player.X ? Player.O : Player.X);
+        }
+
+        //private bool AreSquresMarked()
+
+    }
+}
